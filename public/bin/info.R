3d913547e28b6ad4d73cc731b201fd8824a96b2c
Pierre Tocquin <ptocquin@gmail.com>
2018-10-27 14:41:38 +0200

    minor fix

--- public/bin/info.R
+++ public/bin/info.R
@@ -1,35 +1,15 @@
 #!/usr/local/bin/Rscript
 
-#### Args #################################################
-args         <- commandArgs(TRUE)
-
-# juste pour tester
-if(length(args) == 0) {
-  message("Analyse en mode test...")
-
-} else {
-  message("Info.R")
-  # cluster_id <- args[1]
-  # recipe_id  <- args[2] 
   logfile <- "log.txt"
   uu      <- file(logfile, open = "wt")
   sink(uu, type = "message")
-}
 
 #### Librairies ###########################################
 library("RSQLite")
 source("./bin/config.R")
-# library("data.table")
-
-#### Parameters
-# command.file <- "./bin/commands"
-# db <- "../var/data.db"
-# port     <- "/dev/ttyUSB1"
 
 #### Connexion à la base de données
 con <- dbConnect(SQLite(), dbname = db)
-
-# cat(append = F, file = command.file)
 
 luminaires <-  dbGetQuery(con, paste0("SELECT * FROM luminaire"))
 
