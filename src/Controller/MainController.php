bdae58b4e41cf0568b26060bdb6259f4dae2a4f1
Pierre Tocquin <ptocquin@gmail.com>
2018-10-28 12:27:52 +0100

    minor fix

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -209,8 +209,8 @@ class MainController extends AbstractController
 		$em->flush();
 
 		// Interroger le réseau de luminaires
-    	// $process = new Process('./bin/info.R');
-        $process = new Process('./bin/get_data.sh');
+    	$process = new Process('./bin/info.R');
+        // $process = new Process('./bin/get_data.sh');
 		$process->run();
 
 		// executes after the command finishes
