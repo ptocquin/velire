b1c398d2f2c6a789385cb952ece488864ce765c5
Pierre Tocquin <ptocquin@gmail.com>
2020-07-30 08:24:34 +0200

    assets

--- assets/js/test.js
+++ assets/js/test.js
@@ -3,14 +3,38 @@ $(document).ready(function () {
 	$('#test_recipe').on('click', function(e){
 		labels = [];
 		intensities = [];
+		pwm_start = [];
+		pwm_stop = [];
 		commands = [];
-		$('input').each(function(){
-		   if( $(this).attr('id').match(/ingredients/) ) {
-		   		labels.push($("label[for='" + $(this).attr('id') + "']").text());
-		        intensities.push($(this).val());
-		        commands.push($("label[for='" + $(this).attr('id') + "']").text()+" "+$(this).val());
-		   }
+		$('tbody tr').each(function(i, tr){
+			labels.push($(tr).children('th').text());
 		});
+		$('tbody tr td input ').each(function(i, input){
+			if($(input).attr('id').match(/level/)){
+				intensities.push($(input).val());
+			}
+		});
+		$('tbody tr td input ').each(function(i, input){
+			if($(input).attr('id').match(/pwm_start/)){
+				pwm_start.push($(input).val());
+			}
+		});
+		$('tbody tr td input ').each(function(i, input){
+			if($(input).attr('id').match(/pwm_stop/)){
+				pwm_stop.push($(input).val());
+			}
+		});
+
+		for (var i = labels.length - 1; i >= 0; i--) {
+			commands.push(labels[i]+" "+intensities[i]+" "+pwm_start[i]+" "+pwm_stop[i]);
+		}
+
+		console.log(commands);
+				   // if( $(this).attr('id').match(/ingredients/) ) {
+		   // 		labels.push($("label[for='" + $(this).attr('id') + "']").text());
+		   //      intensities.push($(this).val());
+		   //      commands.push($("label[for='" + $(this).attr('id') + "']").text()+" "+$(this).val());
+		   // }
 
 		d = {labels: labels, intensities: intensities, commands: commands}
 		$.ajax({
